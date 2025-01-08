a436b1d56dcec1c96c03f6e0ebe33504089a2e74
NadiaAhmed15 <nadiaahmedoo15@gmail.com>
2025-01-08 10:35:21 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
- export const SERVER_URL = "https://book-app-server-beige.vercel.app";
+ export const SERVER_URL = "book2-app-server.vercel.app";
 //export const SERVER_URL = "http://localhost:1111";
